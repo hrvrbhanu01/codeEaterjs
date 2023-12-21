ca7032e68d14454bccf93c463d633788a4635f93
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-12-21 14:54:16 +0530

    new

--- server4.js
+++ server4.js
@@ -1,7 +1,21 @@
 const http = require('http') //importing module
-
+const fs=require('fs')
 const server=http.createServer((req,res) => {
-    
+    const url =req.url
+    if(url==='/'){
+    res.setHeader("Content-Type","text/html")
+    res.write("<html>")
+    res.write("<head><title>My First Page</title></head>")
+    res.write("<body><h1>My First Page</h1></body>")
+    res.write("</html>")
+    return res.end()
+ }
+ if (url==="/file"){
+    fs.writeFileSync("NewFile.txt","Hello Brother")
+    res.statusCode=302 //helps in redirect
+    res.setHeader("Loacation", "/")
+    return res.end()
+ }
     res.setHeader("Content-Type","text/html")
     res.write("<html>")
     res.write("<head><title>My First Page</title></head>")
@@ -10,4 +24,4 @@ const server=http.createServer((req,res) => {
     res.end()
 })
 
-server.listen(6000)
+server.listen(3000)
