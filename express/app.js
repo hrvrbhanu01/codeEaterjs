3c147d0a51abc9358be20be5bd51eed1930b0170
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-12-22 12:49:46 +0530

    expresss

--- express/app.js
+++ express/app.js
@@ -1,1 +1,16 @@
 const http=require('http')
+const express = require('express')
+
+const app=express() 
+
+app.use((req,res,next) => {
+    console.log("Middleware-1");
+    next()     //passing to the next middleware 
+})
+
+app.use((req,res,next) => {
+    console.log("Middleware-2");
+    res.send('<h1>Hello it is me express</h1>')  //sending response from the server
+})
+
+app.listen(3000)
