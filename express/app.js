09b2e45a689e81a44fab63f2ecf38dc33e3dd441
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-12-28 11:08:02 +0530

    body-parser use

--- express/app.js
+++ express/app.js
@@ -1,16 +1,25 @@
 const http=require('http')
 const express = require('express')
+const bodyParser=require("body-parser")
 
 const app=express() 
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use((req,res,next) => {
+
+app.get("/", (req,res,next) => {
     console.log("Middleware-1");
-    next()     //passing to the next middleware 
+    console.log(req.url);
+    res.send("<h1>Hello it is me Express</h1>")
+})
+app.get("/abc", (req,res,next) => {
+    console.log("Middleware-2");
+    res.send('<form action="/course" method="POST"><input type="text" name="title"><button type="submit">Add Course</button></form>'
+    );
 })
 
-app.use((req,res,next) => {
-    console.log("Middleware-2");
-    res.send('<h1>Hello it is me express</h1>')  //sending response from the server
+app.post("/course", (req,res,next) => {
+    console.log(req.body);
+    res.redirect("/")
 })
 
 app.listen(3000)
